92df5657df7d75539cdaf01cfd236a546b3d8ab4
Eng. Eman Rezk <eng.eman.rezk@gmail.rezk>
2014-11-09 14:17:09 +0200

    Initial Commit

---
+++ Localize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Localize
+//
+//  Created by Eng. Eman Rezk on 11/9/14.
+//  Copyright (c) 2014 Eng.Eman.Rezk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
